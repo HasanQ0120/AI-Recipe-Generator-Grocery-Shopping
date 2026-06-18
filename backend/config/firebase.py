0b1b839fe07cfb24fc88f0870e46f5a183d0f262
Your Name <you@example.com>
2026-06-18 16:13:37 +0500

    feat: firebase connected to fastapi, backend verified

--- backend/config/firebase.py
+++ backend/config/firebase.py
@@ -4,7 +4,6 @@ import os
 
 def initialize_firebase():
     if not firebase_admin._apps:
-        # This finds the file no matter where you run from
         base_dir = os.path.dirname(os.path.abspath(__file__))
         key_path = os.path.join(base_dir, "serviceAccountKey.json")
         cred = credentials.Certificate(key_path)
